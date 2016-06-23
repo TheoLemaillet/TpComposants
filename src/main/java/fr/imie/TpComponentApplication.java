8f01c01dcfa33b813d896408f62de1d2822ff0a9
Theo Lemaillet <t.lemaillet@gmail.com>
2016-06-23 13:55:15 +0200

    initial commit

---
+++ src/main/java/fr/imie/TpComponentApplication.java
@@ -0,0 +1,12 @@
+package fr.imie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpComponentApplication.class, args);
+	}
+}
